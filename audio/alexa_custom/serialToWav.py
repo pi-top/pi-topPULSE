d602095c0a2fdc3f86e3dc84dba682f0b842d585
Work <mike@pi-top.com>
2017-05-19 10:45:04 +0100

    Updated serialToWav - still not working

--- audio/alexa_custom/serialToWav.py
+++ audio/alexa_custom/serialToWav.py
@@ -8,10 +8,22 @@
 # https://developer.amazon.com/public/solutions/alexa/alexa-voice-service/rest/speechrecognizer-requests
 
 
+# Define functionality here
+debug = True
+capture_sample_rate    = 20050
+capture_bit_rate       = 8
+output_file            = 'output.wav'
+temp_output_file       = 'output.wav.tmp'
+baud_rate              = 250000
+
+
 print("Importing modules...")
 import math
-import serial
 import signal
+
+if not debug:
+    import serial
+
 import struct
 import subprocess
 import sys
@@ -20,15 +32,8 @@ import time
 import tty
 import os
 import select
-import nnresample
+from nnresample import resample
 print("Done...")
-
-# Define functionality here
-debug = True
-capture_sample_rate    = 20050
-capture_bit_rate       = 8
-output_file            = 'output.wav'
-baud_rate              = 250000
 
 
 def signal_handler(signal, frame):
@@ -149,17 +154,18 @@ def init_header_information():
     return header
 
 
-def update_header(file, pos, int_val, byte_len):
+def update_header(file_contents, pos, int_val, byte_len):
     hex_value = spaced_l_endian_hex(int_val, byte_len)
-    file.seek(pos)
-    file.write(hex_to_bytes(hex_value))
+    byte_arr = hex_to_bytes(hex_value)
+    file_contents = file_contents[:pos] + byte_arr + file_contents[(pos + len(byte_arr)):]
+    return file_contents
 
 
 def update_header_information_after_recording():
     # Update zero'd ChunkSize and Subchunk2Size
-    with open(output_file, 'rw+') as file:
-        print("Updating header information...")
-
+    with open(output_file, 'r') as file:
+        new_file_contents = file.read()
+        
         # Calculate DATA
         size_of_data = get_size(output_file) - header_length
         print("Length of DATA: " + str(size_of_data))
@@ -167,32 +173,47 @@ def update_header_information_after_recording():
         if size_of_data <= 0:
             print("No DATA - removing output file!")
             os.remove(output_file)
-            return False
+            success = False
         else:
             Subchunk2Size = size_of_data
             ChunkSize = 36 + Subchunk2Size
 
-            update_header(file, pos = 4, int_val = ChunkSize, byte_len = 4)
-            update_header(file, pos = 40, int_val = Subchunk2Size, byte_len = 4)
-            return True
+            new_file_contents = update_header(new_file_contents, pos = 4, int_val = ChunkSize, byte_len = 4)
+            new_file_contents = update_header(new_file_contents, pos = 40, int_val = Subchunk2Size, byte_len = 4)
+
+            with open(temp_output_file, 'w') as tmp_file:
+                tmp_file.write(new_file_contents)
+
+            success = True
+
+    if success:
+        os.rename(temp_output_file, output_file)
+
+    return success
 
 
 def fix_file_for_alexa():
     print("Done creating WAV file")
     # ONLY RUN IF SAMPLE RATE IS NOT EQUAL TO alexa_reqd_sample_rate
     sample_rate = get_sample_rate_wav_file()
-    if sample_rate != alexa_reqd_sample_rate:
+    bits_per_sample = get_bits_per_sample_wav_file()
+
+    correct_sample_rate = (sample_rate == alexa_reqd_sample_rate)
+    correct_bits_per_sample = (bits_per_sample == alexa_reqd_bits_per_sample)
+    
+
+    if correct_sample_rate == False:
         print("File sample rate: " + str(sample_rate))
         print("Required sample rate: " + str(alexa_reqd_sample_rate))
         print("Resampling...")
-        resample_wav_file()
-        print("Done resampling")
+        # resample_wav_file()
+        print("Done resampling - adjusting bit rate")
+        # adjust_wav_file_bit_rate()
     
-    print("Updating header info for Alexa")
-    update_header_information_for_alexa()
+    update_header_information_for_alexa(update_sample_rate = (correct_sample_rate == False), update_bits_per_sample = (correct_bits_per_sample == False))
 
 
-def get_bit_rate_wav_file():
+def get_bits_per_sample_wav_file():
     resp = subprocess.check_output(['sox', '--info', output_file])
     lines = resp.split("\n")
 
@@ -235,9 +256,18 @@ def get_sample_rate_wav_file():
 
 
 def resample_wav_file():
-    with open(output_file, 'r') as file:
+    with open(output_file, 'rb') as file:
         file_contents = file.read()
-        stripped_file_contents = file_contents[header_length:]
+        file_contents_header_str = file_contents[:header_length]  # Header
+        stripped_file_contents_str = file_contents[header_length:]  # After Header
+        wav_float_arr = []
+        for ch in stripped_file_contents_str:
+            hex_item = format(ord(ch), 'x')
+            float_item = float(float(int(hex_item, 16)) / 127.5) - 1.0
+            # print(str(hex_item).zfill(2) + " : " + str(float_item))
+            wav_float_arr.append(float_item)
+
+    # print(wav_float_arr)
 
     # beta : float
     #     Beta factor for Kaiser window.  Determines tradeoff between
@@ -247,20 +277,95 @@ def resample_wav_file():
     #     the better, at the cost of complexity.
     # axis : int
     #     The axis of `x` that is resampled.
-    resampled_stripped_file_contents = nnresample.resample(stripped_file_contents, alexa_reqd_sample_rate, get_sample_rate_wav_file(), beta = 5.0, L = 16001, axis = 0)
+    resampled_wav_float_arr = resample(wav_float_arr, alexa_reqd_sample_rate, get_sample_rate_wav_file(), beta = 5.0, L = 16001, axis = 0)
 
-    with open(output_file, 'w') as file:
+    # resampled_stripped_file_contents = sig.resample(wav_float_arr, alexa_reqd_sample_rate)
+
+    resampled_stripped_file_contents = "";
+    for float_item in resampled_wav_float_arr:
+        # Convert back to int
+        # -1 to +1 -> 0 to 255
+        int_item = int(round((float_item + 1) * 127.5))
+        
+        # Catch value overflow from resampling
+        if int_item < 0:
+            int_item = 0
+        elif int_item > 255:
+            int_item = 255
+        
+        # int to byte
+        resampled_stripped_file_contents += hex_to_bytes(spaced_l_endian_hex(int_val = int_item, byte_len = 1))
+
+    print("Writing to " + temp_output_file)
+    with open(temp_output_file, 'wb') as file:
+        file.write(file_contents_header_str)
         file.write(resampled_stripped_file_contents)
 
+    print("Moving " + temp_output_file + " to " + output_file)
+    os.rename(temp_output_file, output_file)
 
-def update_header_information_for_alexa():
-    print("Done resampling - updating bits per sample to 16")
-    with open(output_file, 'rw+') as file:
-        ByteRate = alexa_reqd_sample_rate * alexa_reqd_bits_per_sample / 8  # ByteRate         == SampleRate * BitsPerSample/8
-        BlockAssign = alexa_reqd_bits_per_sample / 8                        # BlockAlign       == BitsPerSample/8
-        BitsPerSample = alexa_reqd_bits_per_sample                          # BitsPerSample
-        update_header(file, pos = 28, int_val = ByteRate, byte_len = 4)
-        update_header(file, pos = 34, int_val = BitsPerSample, byte_len = 2)
+
+
+def update_header(file_contents, pos, int_val, byte_len):
+    hex_value = spaced_l_endian_hex(int_val, byte_len)
+    byte_arr = hex_to_bytes(hex_value)
+    file_contents = file_contents[:pos] + byte_arr + file_contents[(pos + len(byte_arr)):]
+    return file_contents
+
+
+
+def adjust_wav_file_bit_rate():
+    # PAD WAV CONTENT
+    with open(output_file, 'r') as file:
+        with open(temp_output_file, 'w') as tmp_file:
+            reading = True
+            header_info = file.read(header_length)
+            tmp_file.write(header_info)
+            
+            while reading:
+                wav_byte_s = file.read(1)
+                if not wav_byte_s:
+                    reading = False
+                else:
+                    wav_byte = wav_byte_s[0]
+                    
+                    # wav_val = int(wav_byte.encode('hex'), 16)
+                    # scaled_wav_val = wav_val * 65535 / 255
+                    # wav_bytes_to_write = hex_to_bytes(spaced_l_endian_hex(scaled_wav_val, byte_len = 2))
+                    # tmp_file.write(wav_bytes_to_write)
+
+                    wav_bytes_to_write = hex_to_bytes(spaced_l_endian_hex(0, byte_len = 1)) + wav_byte
+                    tmp_file.write(wav_bytes_to_write)
+
+    os.rename(temp_output_file, output_file)
+    return
+
+
+def update_header_information_for_alexa(update_sample_rate = False, update_bits_per_sample = False):
+    if update_sample_rate or update_bits_per_sample:
+        print("Setting 'bits per sample' to 16 and sample rate to 16kHz")
+        with open(output_file, 'r') as file:
+            new_file_contents = file.read()
+
+            if update_sample_rate:
+                SampleRate = alexa_reqd_sample_rate                                   # SampleRate
+                new_file_contents = update_header(new_file_contents, pos = 24, int_val = SampleRate, byte_len = 4)
+
+            if update_sample_rate or update_bits_per_sample:
+                ByteRate = alexa_reqd_sample_rate * (alexa_reqd_bits_per_sample / 8)  # ByteRate         == SampleRate * BitsPerSample/8
+                new_file_contents = update_header(new_file_contents, pos = 28, int_val = ByteRate, byte_len = 4)
+
+            if update_bits_per_sample:
+                BlockAssign = alexa_reqd_bits_per_sample / 8                          # BlockAlign       == BitsPerSample/8
+                new_file_contents = update_header(new_file_contents, pos = 32, int_val = BlockAssign, byte_len = 2)
+
+                BitsPerSample = alexa_reqd_bits_per_sample                            # BitsPerSample
+                new_file_contents = update_header(new_file_contents, pos = 34, int_val = BitsPerSample, byte_len = 2)
+
+            with open(temp_output_file, 'w') as tmp_file:
+                tmp_file.write(new_file_contents)
+
+        os.rename(temp_output_file, output_file)
 
 
 def send_to_alexa():
@@ -268,13 +373,13 @@ def send_to_alexa():
     subprocess.call(['./requestAlexa.sh'])
 
 def get_response_from_alexa():
-    with open('response.txt', 'r') as file:
+    with open('response.txt', 'rb') as file:
         resp = file.read()
     print(resp)
 
 
 def save_wav_from_serial():
-    with open(output_file, 'w') as file:
+    with open(output_file, 'wb') as file:
         print("WRITING: initial header information")
         file.write(init_header_information())
         print("WRITING: wave data")
@@ -299,20 +404,16 @@ def save_wav_from_serial():
 
 
 def do_alexa():
-    print("Updating header information")
-    if update_header_information_after_recording():
-        print("Done creating WAV file - fixing for Alexa")
-        fix_file_for_alexa()
+    print("Fixing WAV for Alexa")
+    fix_file_for_alexa()
 
-        print("Posting to Alexa")
-        send_to_alexa()
+    print("Posting to Alexa")
+    send_to_alexa()
 
-        print("Sent to Alexa - reading response")
-        get_response_from_alexa()
+    print("Sent to Alexa - reading response")
+    get_response_from_alexa()
 
-        print("Done")
-    else:
-        print("Error - failed to fix header information")
+    print("Done")
 
 
 # Internal variables
@@ -333,5 +434,9 @@ else:
         update_state_from_keypress()
         if save_to_file:
             save_wav_from_serial()
-            do_alexa()
+            print("Updating header information")
+            if update_header_information_after_recording():
+                do_alexa()
+            else:
+                print("Error - failed to fix header information")
             break
