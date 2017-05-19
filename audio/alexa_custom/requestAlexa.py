e8d87eef966c573a29e3c0db827f54d5d81e4b4e
Work <mike@pi-top.com>
2017-05-19 13:20:57 +0100

    Ported requestAlexa.sh to requestAlexa.py

--- audio/alexa_custom/requestAlexa.py
+++ audio/alexa_custom/requestAlexa.py
@@ -1,18 +1,20 @@
-import subprocess
-import sys
+import json
 import os
 import requests
+import subprocess
+import sys
 
 ENV_PATH = os.path.dirname(os.path.abspath(__file__)) + "/auth/env"
 sys.path.append(ENV_PATH)
 
-#########
-# SETUP #
-#########
 DIR = os.path.dirname(os.path.realpath(__file__))
 
-AUDIO_FILENAME = DIR + "/audio/output.wav"
-METADATA_FILENAME = DIR + "/metadata.json"
+AUDIO_FILENAME = "output.wav"
+AUDIO_FILEPATH = DIR + "/audio/" + AUDIO_FILENAME
+METADATA_FILENAME = "metadata.json"
+METADATA_FILEPATH = DIR + "/" + METADATA_FILENAME
+RESPONSE_FILENAME = "response.txt"
+RESPONSE_FILEPATH = DIR + "/" + RESPONSE_FILENAME
 
 print("Getting new auth token...")
 subprocess.call(DIR + "/auth/refresh.sh")
@@ -28,90 +30,41 @@ except NameError:
 	print("No access token - go through auth process")
 	sys.exit(1)
 else:
-	print("Access Token:")
-	print(auth.ACCESS_TOKEN)
-
-print("Done.")
+	print("Done")
 
 
-##########
-# UPLOAD #
-##########
 print("Uploading audio to Alexa...")
+endpoint = "https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize"
 
-subprocess.call(["curl", "-s", "-i",
-	"-H", "Authorization: Bearer " + auth.ACCESS_TOKEN,
-	"-F", "metadata=<" + METADATA_FILENAME + ";type=application/json;", "charset=UTF-8",
-	"-F", "audio=<" + AUDIO_FILENAME + ";type=audio/L16;", "rate=16000;", "channels=1",
-	"https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize",
-	"-o", "response.txt"])
+r = requests.post(
+	endpoint
+	, headers={
+		"Authorization": "Bearer " + auth.ACCESS_TOKEN
+	}
+	, files={
+		'metadata': (METADATA_FILENAME, open(METADATA_FILEPATH, 'rb'), 'application/json; charset=UTF-8'),
+		'audio': (AUDIO_FILENAME, open(AUDIO_FILEPATH, 'rb'), 'audio/L16; rate=16000; channels=1')
+	}
+)
 
-# endpoint = "https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize"
-
-# r = requests.post(
-# 	endpoint,
-# 	headers={
-# 		"Authorization": "Bearer " + auth.ACCESS_TOKEN
-# 	},
-# 	forms={
-# 		# ???
-# 	},
-# 	files={
-# 		# ???
-# 	}
-# )
-
-# with open('response.txt', 'wb') as handle:
-#     for block in response.iter_content(1024):
-#         handle.write(block)
-
-print("Done.")
-
-
-
-
-
-
-# responseFile="$DIR/response.txt"
-
-# echo "Processing Alexa response..."
-# echo "$resp" | validjson &> /dev/null
-# valid_json=$?
-# if [ $valid_json -eq 0 ]; then
-# 	ERROR=$(echo "$resp" | jq '.error.message')
-
-# 	if [[ "$ERROR" != "null" ]]; then
-# 		echo "Error: $ERROR"
-# 		if [[ -f "$responseFile" ]]; then
-# 			rm "$responseFile"
-# 		fi
-# 	else
-# 		echo "JSON response: $resp"
-# 	fi
-# else
-# 	cat "$responseFile" | http-message-parser --pick=multipart[1].body | mpg123 -
-# fi
-
-
-
-
-
-###################
-# HANDLE RESPONSE #
-###################
-RESPONSE_FILENAME = DIR + "/response.txt"
-
+if r.status_code != 200:
+	print("ERROR!")
+	resp = r.json()
+	error_obj = resp['error']
+	error_code = error_obj['code']
+	error_msg = error_obj['message']
+	print("\t" + error_code)
+	print("\t" + error_msg)
+	sys.exit()
+	
 print("Processing Alexa response...")
 
-# echo "$resp" | validjson &> /dev/null
-# valid_json=$?
-# if [ $valid_json -eq 0 ]; then
-	# ERROR=$(echo "$resp" | jq '.error.message')
-	if ERROR != "null":
-		print("Error: $ERROR")
-		if os.path.isfile(RESPONSE_FILENAME):
-			os.remove(RESPONSE_FILENAME)
-	else:
-		print("JSON response: $resp")
-# else:
-	subprocess.call(["cat", RESPONSE_FILENAME, "|", "http-message-parser", "--pick=multipart[1].body", "|", "mpg123", "-"])
+with open(RESPONSE_FILEPATH, 'wb') as handle:
+    for block in r.iter_content(1024):
+        handle.write(block)
+
+cat = subprocess.Popen(('cat', RESPONSE_FILEPATH), stdout=subprocess.PIPE)
+hmp = subprocess.Popen(('http-message-parser', '--pick=multipart[1].body'), stdin=cat.stdout, stdout=subprocess.PIPE)
+output = subprocess.call(('mpg123', '-'), stdin=hmp.stdout)
+cat.wait()
+hmp.wait()
