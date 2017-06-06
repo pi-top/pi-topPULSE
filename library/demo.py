4d9b335d26757ce46e650c05e8d8a422041080b7
Ben P <ben@pi-top.com>
2017-06-06 18:02:34 +0100

    First commit of mic module

--- library/demo.py
+++ library/demo.py
@@ -1,13 +1,14 @@
 # demo.py
 
 import ptpulse
-from time import sleep 
+import time
 
 led_matrix = ptpulse.ledmatrix
-led_matrix.set_all(255,255,255)
+led_matrix.set_all(144,144,144)
 led_matrix.show()
 
 mic = ptpulse.microphone
 mic.record()
 time.sleep(5)
 mic.stop()
+mic.save("/tmp/test.wav", True)
