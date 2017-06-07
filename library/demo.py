c3fb57ad2f503c632f30e4552eddf151d15327cc
Ben P <ben@pi-top.com>
2017-06-07 11:16:32 +0100

    Restore demo

--- library/demo.py
+++ library/demo.py
@@ -4,9 +4,9 @@ from ptpulse import ledmatrix
 from ptpulse import microphone
 import time
 
-#ledmatrix.run_tests()
+ledmatrix.run_tests()
 
 microphone.record()
-time.sleep(5)
+time.sleep(3)
 microphone.stop()
 microphone.save("/tmp/test.wav", True)
