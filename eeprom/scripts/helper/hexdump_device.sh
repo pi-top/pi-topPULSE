7dd7bead2a5c9e4e2f4cf03ce0213c602b79e834
Matt Simons <matt@pi-top.com>
2017-06-20 11:27:10 +0100

    neutralise eeprom

--- eeprom/scripts/helper/hexdump_device.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-sudo xxd /sys/class/i2c-adapter/i2c-0/0-0050/eeprom
--- eeprom/scripts/helper/hexdump_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARENT="$(dirname $DIR)"
-ROOT="$(dirname $PARENT)"
-
-EEPROM_BENCHMARK_OUTPUT_FILE="$ROOT/bin/benchmark.eep"
-
-xxd $EEPROM_BENCHMARK_OUTPUT_FILE
