653bab4101c05b32061f881ce6e29cd1d81dfd76
Work <mike@pi-top.com>
2017-06-20 11:54:26 +0100

    Removed audio folder

--- audio/alexa_custom/configure_auth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-"$DIR/helper/setup_get_auth_code_url.sh"
-"$DIR/helper/setup_get_auth_and_refresh_tokens.sh"
--- audio/alexa_custom/env/common.py
+++
@@ -1,4 +0,0 @@
-CLIENT_ID="amzn1.application-oa2-client.bcbbf90ce9474117bb8785cb38cee03e"
-CLIENT_SECRET="a686e4e1865b75c52ff36d8ce998fed8ec2771e8ba29e95cffe11a52fa1c3be8"
-REDIRECT_URI="https://localhost:9745/authresponse"
-DEVICE_TYPE_ID="test_device"
