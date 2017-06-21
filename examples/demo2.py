bd6b14ecbcf9d5e65ee4f0bc2ad30e29e562019a
Work <mike@pi-top.com>
2017-06-21 20:09:18 +0100

    Updated demo2.py to no longer enable/disable the device

--- examples/demo2.py
+++ examples/demo2.py
@@ -5,68 +5,61 @@ from ptpulse import microphone
 from ptpulse import configuration
 import time
 
-print ("Enabling device...")
-configuration.reset_device_state(True)
-print ("")
-
-time.sleep(1)
-
-print ("Setting sample rate to 22KHz...")
+print("Setting sample rate to 22KHz...")
 configuration.set_microphone_sample_rate_to_22khz()
 
 time.sleep(1)
 
-print ("Recording audio for 5s...")
+print("Setting bit rate to 16...")
+microphone.set_bit_rate_to_signed_16()
+
+
+time.sleep(1)
+
+print("Recording audio for 5s...")
 ledmatrix.set_all(255, 0, 0)
 ledmatrix.show()
 microphone.record()
 time.sleep(5)
 microphone.stop()
 microphone.save("/tmp/test22-1.wav", True)
-print ("Saved to /tmp/test22-1.wav")
-print ("")
+print("Saved to /tmp/test22-1.wav")
+print("")
 
 ledmatrix.off()
 time.sleep(1)
 
-print ("Setting sample rate to 16KHz...")
+print("Setting sample rate to 16KHz...")
 configuration.set_microphone_sample_rate_to_16khz()
 
 time.sleep(1)
 
-print ("Recording audio for 5s...")
+print("Recording audio for 5s...")
 ledmatrix.set_all(255, 0, 0)
 ledmatrix.show()
 microphone.record()
 time.sleep(5)
 microphone.stop()
 microphone.save("/tmp/test16.wav", True)
-print ("Saved to /tmp/test16.wav")
-print ("")
+print("Saved to /tmp/test16.wav")
+print("")
 
 ledmatrix.off()
 time.sleep(1)
 
-print ("Setting sample rate to 22KHz...")
+print("Setting sample rate to 22KHz...")
 configuration.set_microphone_sample_rate_to_22khz()
 
 time.sleep(1)
 
-print ("Recording audio for 5s...")
+print("Recording audio for 5s...")
 ledmatrix.set_all(255, 0, 0)
 ledmatrix.show()
 microphone.record()
 time.sleep(5)
 microphone.stop()
 microphone.save("/tmp/test22-2.wav", True)
-print ("Saved to /tmp/test22-2.wav")
-print ("")
+print("Saved to /tmp/test22-2.wav")
+print("")
 
 ledmatrix.off()
-
-time.sleep(1)
-
-print("Disabling device...")
-configuration.reset_device_state(False)
-print ("")
-
